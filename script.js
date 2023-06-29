29ce284618f9893415eba02f3e4879dd9c503918
Alexander Yordanov <105381567+FireWolf240Playz@users.noreply.github.com>
2023-06-29 20:37:14 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,100 @@
 "use strict";
-let playing = true;
+const message = document.querySelector(".message");
+const check = document.querySelector(".check");
+const again = document.querySelector(".again");
+const number = document.querySelector(".number");
+const displayScore = document.querySelector(".score");
+const highScore = document.querySelector(".high-score");
+const guess = Number(document.querySelector(".guess").value);
+let generatedNumber = Math.trunc(Math.random() * 20) + 1;
+let isPlayable = true;
 let score = 20;
-let highScore = 0;
-let secretNumber = Math.trunc(Math.random() * 20 + 1);
+//Functions
 
-const displayMessage = function (message) {
-  document.querySelector(".message").textContent = message;
+const deductPoints = function () {
+  score--;
+  displayScore.textContent = score;
 };
 
-document.querySelector(".check").addEventListener("click", function () {
-  console.log(secretNumber);
-  const guess = Number(document.querySelector(".guess").value);
+const winGame = function () {
+  message.textContent = "You guessed the right number!";
+  highScore.textContent = score;
+  document.body.style.backgroundColor = "#60b347";
+  isPlayable = false;
+  number.style.width = "30rem";
+  number.textContent = generatedNumber;
+};
 
-  if (!guess) {
-    displayMessage("Please enter a number between 1-20");
-    score--;
-    document.querySelector(".score").textContent = score;
-  } else if (guess === secretNumber) {
-    displayMessage("Correct Number! Bravo!");
-    document.querySelector(".number").textContent = secretNumber;
+const tooHigh = function () {
+  message.textContent = "Too high";
+  deductPoints();
+};
 
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector(".number").style.width = "30rem";
+const tooLow = function () {
+  message.textContent = "Too low";
+  deductPoints();
+};
 
-    if (score > highScore) {
-      highScore = score;
+const loseGame = function () {
+  isPlayable = false;
+  message.textContent = "You lost the game";
+};
 
-      document.querySelector(".highscore").textContent = highScore;
-    }
-  } else if (guess !== secretNumber) {
-    if (score > 0) {
-      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      displayMessage("💥 You lost the game!");
-      document.querySelector(".score").textContent = 0;
-    }
-  }
-});
-
-document.querySelector(".again").addEventListener("click", function () {
+const reset = function () {
+  isPlayable = true;
+  message.textContent = "Start guessing...";
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20 + 1);
-  playing = true;
-  displayMessage("Start guessing...");
-  document.querySelector(".score").textContent = score;
+  displayScore.textContent = score;
+  number.textContent = "?";
+  document.body.style.backgroundColor = "rgb(15, 15, 15)";
+  generatedNumber = Math.trunc(Math.random() * 20) + 1;
+};
 
-  document.querySelector(".guess").value = "";
-  document.querySelector(".number").textContent = "?";
+const enterValidNumber = function () {
+  message.textContent = "Plese enter an number between 1-20";
+  deductPoints();
+};
 
-  document.querySelector(".number").style.width = "15rem;";
+//Game logic
+const checkNumber = function () {
+  const guess = Number(document.querySelector(".guess").value);
+  number.textContent = generatedNumber;
 
-  random_bg_color = function () {
-    var x = Math.floor(Math.random() * 256);
-    var y = Math.floor(Math.random() * 256);
-    var z = Math.floor(Math.random() * 256);
-    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+  if (score > 0 && isPlayable) {
+    if (!guess) {
+      enterValidNumber();
+    } else if (guess === generatedNumber) {
+      winGame();
+      return;
+    } else if (guess > generatedNumber) {
+      tooHigh();
+    } else if (guess < generatedNumber) {
+      tooLow();
+    }
+  } else {
+    loseGame();
+  }
+};
+const randomColor = function () {
+  const generateRandomValue = () => Math.floor(Math.random() * 256);
+  const x = generateRandomValue();
+  const y = generateRandomValue();
+  const z = generateRandomValue();
+  const bgColor = `rgb(${x}, ${y}, ${z})`;
 
-    document.body.style.background = bgColor;
-  };
-  random_bg_color();
-});
+  document.body.style.background = bgColor;
+};
+//Event listners
+check.addEventListener("click", checkNumber);
+again.addEventListener("click", reset);
+again.addEventListener("click", randomColor);
+
+document.addEventListener("keydown", logKey);
+function logKey(e) {
+  if (`${e.code}` == "Escape") {
+    reset();
+  }
+  if (`${e.code}` == "Enter") {
+    checkNumber();
+  }
+}
